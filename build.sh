659bf2a6706ebb481585a2a3dfa27bd58696a7ab
Tamer Gur <tamerg01@gmail.com>
2019-11-24 19:39:20 +0000

    new configurations with gff3 and taxonomy mappings also new release binary  will include default datasets update data

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo " Version paramater is requried."
+    exit 1
+fi
+
+rm -f biobtree_MacOS_64bit.tar.gz
+
+bbLatestVersion=`curl -Ls -o /dev/null -w %{url_effective} https://github.com/tamerh/biobtree/releases/latest | rev | cut -d '/' -f 1 | rev`
+
+curl -OL https://github.com/tamerh/biobtree/releases/download/$bbLatestVersion/biobtree_MacOS_64bit.tar.gz 
+
+tar -xzvf biobtree_MacOS_64bit.tar.gz
+
+./biobtree -d hgnc,uniprot,hmdb,taxonomy,go,efo,eco,chebi,interpro --keep --idx cache$1 update
+
+rm biobtree
+rm biobtree_MacOS_64bit.tar.gz
+
+tar --exclude 'biobtree-conf/.git' --exclude 'biobtree-conf/build.sh' --exclude 'biobtree-conf/.gitignore' --exclude 'biobtree-conf/notes.txt' -zcvf biobtree-conf-0.99.1.tar.gz biobtree-conf 
+
